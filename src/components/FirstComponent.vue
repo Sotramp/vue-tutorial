4144912105f51ad80a915d3715a498fdc9c1e3be
sotramp <sotraamp@gmail.com>
2020-02-18 14:25:35 +0100

    End of the tutorial

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <span>Hello {{ msg }}</span>
+        <p>Test de la fonction : {{ onlyFirstWord }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            msg: "World test",
+        }
+    },
+    computed: {
+        onlyFirstWord(){
+            return this.msg.split(' ')[0].toUpperCase();
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
