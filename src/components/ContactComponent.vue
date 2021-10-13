6144513899a80b9d0684a5440c5cd10c10890ce5
hisakiyo <hisakiyo.games@gmail.com>
2021-10-13 13:23:56 +0200

    adding contact page

---
+++ src/components/ContactComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="contact">
+        <h1>Contact</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque euismod, urna eu tincidunt consectetur,
+            nisi nisl aliquam nunc, eu aliquet nisl nunc eu nisl.
+            Nulla facilisi.
+        </p>
+    </div>
+</template>
+
+<script> 
+    export default {
+        name: 'ContactComponent'
+    }
+</script>
+
+<style>
+    .contact {
+        padding: 20px;
+        background-color: #f5f5f5;
+    }
+</style>
